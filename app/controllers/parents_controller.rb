41912ea61d31247241c243fe735debce7de8ed9c
Matt Kragen <mattdkragen@gmail.com>
2021-07-07 17:08:08 -0700

    Add methods to controller

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,4 +1,9 @@
 class ParentsController < ApplicationController
   def index
+    @grocery_stores = GroceryStore.all
+  end
+
+  def show
+    @grocery_store = GroceryStore.find(params[:id])
   end
 end
