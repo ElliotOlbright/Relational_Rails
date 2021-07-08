51359c970009253fd2d493391eb5bf7431f14a6b
Matt Kragen <mattdkragen@gmail.com>
2021-07-07 17:38:04 -0700

    Complete MVC for child #show

--- app/controllers/concerns/children_controller.rb
+++ app/controllers/concerns/children_controller.rb
@@ -2,4 +2,8 @@ class ChildrenController < ApplicationController
   def index
     @groceries = Grocery.all
   end
+
+  def show
+    @grocery = Grocery.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/parents', to: 'parents#index'
   get '/parents/:id', to: 'parents#show'
   get '/child_table_name', to: 'children#index'
+  get '/child_table_name/:id', to: 'children#show'
 end
