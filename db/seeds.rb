06c1502d99fb226b6de878b53fffd966a2183a55
Elliot Olbright <elliotolbright@gmail.com>
2021-07-10 13:19:44 -0600

    update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,30 +2,42 @@
 Car.destroy_all
 Dealership.destroy_all
 
-dealer1 = Dealership.create!(name: 'Dealer1', address: '4200 high st', open_weekends: true, years_open: 42)
-dealer2 = Dealership.create!(name: 'Dealer2', address: '1234 fake st', open_weekends: true, years_open: 12)
-dealer3 = Dealership.create!(name: 'Dealer3', address: '6969 date st', open_weekends: false, years_open: 69)
-dealer4 = Dealership.create!(name: 'Dealer4', address: '2020 corona blvd', open_weekends: false, years_open: 20)
-dealer5 = Dealership.create!(name: 'Dealer5', address: '1600 pennsylvania ave', open_weekends: true, years_open: 16)
-dealer6 = Dealership.create!(name: 'Dealer6', address: '5050 chance cr', open_weekends: false, years_open: 50)
+dealer1 = Dealership.create!(name: 'Lit Motors', address: '4200 high st', open_weekends: true, years_open: 42)
+dealer2 = Dealership.create!(name: 'Elways Dealership', address: '1234 fake st', open_weekends: true, years_open: 12)
+dealer3 = Dealership.create!(name: 'Sweet Rides', address: '6943 date st', open_weekends: false, years_open: 69)
+dealer4 = Dealership.create!(name: 'Sick Cruises', address: '2020 corona blvd', open_weekends: false, years_open: 20)
+dealer5 = Dealership.create!(name: 'White House Dealers', address: '1600 pennsylvania ave', open_weekends: true, years_open: 16)
+dealer6 = Dealership.create!(name: 'Could be motors', address: '5050 chance cr', open_weekends: false, years_open: 50)
 
 dealer1.cars.create!(model: 'WRX', make: 'Subaru', year: 2013, under_100k_miles: true)
 dealer1.cars.create!(model: 'Outback', make: 'Subaru', year: 1998, under_100k_miles: false)
+dealer1.cars.create!(model: 'Forester', make: 'Subaru', year: 2002, under_100k_miles: false)
+dealer1.cars.create!(model: 'impreza', make: 'Subaru', year: 2012, under_100k_miles: true)
 
 dealer2.cars.create!(model: 'MDX', make: 'Acura', year: 2006, under_100k_miles: false)
-dealer2.cars.create!(model: 'MLR', make: 'Acura', year: 2021, under_100k_miles: true)
+dealer2.cars.create!(model: 'MLR', make: 'Acura', year: 1989, under_100k_miles: true)
+dealer2.cars.create!(model: 'TLX', make: 'Acura', year: 2009, under_100k_miles: true)
+dealer2.cars.create!(model: 'RLX', make: 'Acura', year: 2001, under_100k_miles: true)
 
 dealer3.cars.create!(model: 'x1', make: 'BMW', year: 2017, under_100k_miles: true)
 dealer3.cars.create!(model: 'm3', make: 'BMW', year: 1987, under_100k_miles: false)
+dealer3.cars.create!(model: '325i', make: 'BMW', year: 1992, under_100k_miles: false)
+dealer3.cars.create!(model: 'Z4', make: 'BMW', year: 2013, under_100k_miles: true)
 
 dealer4.cars.create!(model: 'a7', make: 'Audi', year: 2009, under_100k_miles: true)
 dealer4.cars.create!(model: 'r8', make: 'Audi', year: 2019, under_100k_miles: true)
+dealer4.cars.create!(model: 'q3', make: 'Audi', year: 2009, under_100k_miles: false)
+dealer4.cars.create!(model: 'a3', make: 'Audi', year: 1999, under_100k_miles: false)
 
 dealer5.cars.create!(model: 'Konna', make: 'Hyundai', year: 2000, under_100k_miles: false)
 dealer5.cars.create!(model: 'Accent', make: 'Hyundai', year: 2015, under_100k_miles: true)
+dealer5.cars.create!(model: 'Sante Fe', make: 'Hyundai', year: 2006, under_100k_miles: true)
+dealer5.cars.create!(model: 'Sonata', make: 'Hyundai', year: 2003, under_100k_miles: false)
 
 dealer6.cars.create!(model: 'model s', make: 'Tesla', year: 2012, under_100k_miles: true)
 dealer6.cars.create!(model: 'cybertruck', make: 'Tesla', year: 2022, under_100k_miles: true)
+dealer6.cars.create!(model: 'model 3', make: 'Tesla', year: 2016, under_100k_miles: true)
+dealer6.cars.create!(model: 'model x', make: 'Tesla', year: 2013, under_100k_miles: false)
 
 #Grocery Store and Groceries Seed
 Grocery.destroy_all
