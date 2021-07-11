f3207920c277380b263d0ce722d4fd0359bab0d0
Matt Kragen <mattdkragen@gmail.com>
2021-07-11 15:24:18 -0700

    Correct test

--- spec/features/grocery_stores/show_spec.rb
+++ spec/features/grocery_stores/show_spec.rb
@@ -82,24 +82,4 @@ describe 'Grocery Stores page' do
     expect(current_path).to eq("/grocery_stores/#{store1.id}/edit")
     expect(page).to have_content('Update Grocery Store')
   end
-
-  it 'can update the attributes of a store' do
-    store1 = GroceryStore.create!(
-      name: 'Albertsons', 
-      address: '1234 Fake Street', 
-      open_24_hours: false
-    )
-    visit "grocery_stores/#{store1.id}"
-    click_link('Update Grocery Store')
-    expect(current_path).to eq("/grocery_stores/#{store1.id}/edit")
-
-    fill_in :name, with: "Example Name"
-    fill_in :address, with: "Example Address"
-    fill_in :open_24_hours, with: "true"
-    click_button('Update Store')
-
-    expect(current_path).to eq("/grocery_stores/#{store1.id}")
-    expect(page).to have_content("Example Name")
-    expect(page).to have_content("Example Address")
-  end
 end 
