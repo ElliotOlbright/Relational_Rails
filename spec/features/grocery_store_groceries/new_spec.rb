be2cb5ac76a7bff7618a3a3f80426278bccc4173
Matt Kragen <mattdkragen@gmail.com>
2021-07-11 15:00:35 -0700

    Add test

--- spec/features/grocery_store_groceries/new_spec.rb
+++ spec/features/grocery_store_groceries/new_spec.rb
@@ -1,6 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Create New Grocery page' do
+  it 'can take the user to the Grocery Store page' do 
+    store1 = GroceryStore.create!(
+      name: 'Albertsons', 
+      address: '1234 Fake Street', 
+      open_24_hours: false
+    )
+
+    store2 = GroceryStore.create!(
+      name: 'Stater Bros', 
+      address: '2345 Chump Boulevard', 
+      open_24_hours: true
+    )
+
+    visit "/grocery_stores/#{store1.id}/groceries"
+  
+    click_link('Go to Grocery Stores')
+    expect(current_path).to eq('/grocery_stores')
+  end  
+  
   it 'lists fields' do
     store1 = GroceryStore.create!(
       name: 'Albertsons', 
